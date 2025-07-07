5623fc5cbe2c8b92405485a2e54d8bc125cda3c3
Kaua <kauacz04coc@gmail.com>
2025-07-06 23:18:02 -0300

    Ajustando NAV para dispositivos moveis

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -4,14 +4,13 @@ import Logo from "./assets/logo.png";
 import React, { useState, useEffect } from "react";
 
 export function Nav() {
-   const [totalFavoritos, setTotalFavoritos] = useState(0); // Atualiza o total de favoritos a partir do localStorage
+   const [totalFavoritos, setTotalFavoritos] = useState(0);
    function atualizarTotalFavoritos() {
       const favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
       setTotalFavoritos(favoritos.length);
    }
 
-   // Atualiza totalFavoritos quando a Home montar
-   React.useEffect(() => {
+   useEffect(() => {
       atualizarTotalFavoritos();
       function onFavoritosAtualizados() {
          atualizarTotalFavoritos();
@@ -24,27 +23,26 @@ export function Nav() {
          );
       };
    }, []);
+
    return (
-      <div className="text-amber-600 font-semibold flex justify-around md:px-50 md:h-17 h-26 bg-white shadow-xs shadow-amber-600/50">
-         <div className="flex md:scale-80 scale-50">
-            <img src={Logo} alt="" />
+      <div className="text-amber-600 font-semibold flex items-center justify-between px-4 md:px-32 w-full h-17 bg-white shadow-xs shadow-amber-600/50">
+         <div className="flex items-center">
+            <img src={Logo} alt="Logo Caminho Boho" className="h-10 md:h-14" />
          </div>
-         <ul className="md:flex gap-8 items-center hidden">
+         <ul className="hidden md:flex gap-4 md:gap-8 items-center text-base md:text-lg">
             <li className="cursor-pointer hover:text-amber-500">Inicio</li>
             <li className="cursor-pointer hover:text-amber-500">Produtos</li>
             <li className="cursor-pointer hover:text-amber-500">Categorias</li>
             <li className="cursor-pointer hover:text-amber-500">Sobre</li>
             <li className="cursor-pointer hover:text-amber-500">Contato</li>
          </ul>
-         <div className="flex items-center gap-15 md:text-2xl text-5xl md:gap-10">
+         <div className="flex items-center gap-4 md:gap-10 text-2xl md:text-3xl">
             <div className="relative">
                <IoHeartOutline className="cursor-pointer" />
-               {totalFavoritos >= 1 ? (
-                  <span className="absolute -top-2 -right-3 text-white bg-amber-500 text-sm md:text-xs p-2 md:p-1 rounded-full">
+               {totalFavoritos >= 1 && (
+                  <span className="absolute -top-2 -right-3 text-white bg-amber-500 text-xs p-1 rounded-full">
                      {totalFavoritos}
                   </span>
-               ) : (
-                  ""
                )}
             </div>
             <RiShoppingCartFill className="cursor-pointer" />
