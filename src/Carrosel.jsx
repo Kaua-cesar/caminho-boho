df8722991c68c6603386b682d89ea387135aa904
Kaua <kauacz04coc@gmail.com>
2025-07-07 00:58:14 -0300

    Corrijido Carossel para dispositivos mobile

--- src/Carrosel.jsx
+++ src/Carrosel.jsx
@@ -29,18 +29,20 @@ export function Carrosel() {
             className="mySwiper"
          >
             <SwiperSlide>
-               <div className="absolute flex flex-col items-start top-[40%] left-45 text-white">
-                  <p className="text-2xl font-semibold">
+               <div className="absolute flex flex-col items-start top-[20%] md:top-[40%] md:left-45 text-white">
+                  <p className="text-2xl font-semibold w-full md:w-auto">
                      Para ocasioes especiais
                   </p>
-                  <h1 className="text-6xl font-bold">Costa nua curto</h1>
-                  <h2 className="text-xl">
+                  <h1 className="text-6xl font-bold w-full">Costa nua curto</h1>
+                  <h2 className="text-xl w-full md:w-auto md:mb-0 ">
                      Vestidos de costa falase opasdklaskdlas
                   </h2>
-                  <ButtonsCarrosel
-                     button1Text="Ver coleção"
-                     button2Text="Saiba mais sobre Costa nua"
-                  />
+                  <div className="w-full items-center justify-center">
+                     <ButtonsCarrosel
+                        button1Text="Ver coleção"
+                        button2Text="Saiba mais sobre Costa nua"
+                     />
+                  </div>
                </div>
                <img
                   src="https://png.pngtree.com/background/20250104/original/pngtree-woman-in-flowing-gown-at-sunset-cityscape-background-picture-image_16103168.jpg"
@@ -48,16 +50,20 @@ export function Carrosel() {
                />
             </SwiperSlide>
             <SwiperSlide>
-               <div className="absolute flex flex-col items-start top-[40%] left-45 text-white">
-                  <p className="text-2xl font-semibold">Para o dia a dia</p>
-                  <h1 className="text-6xl font-bold">Viscose liso</h1>
-                  <h2 className="text-xl">
+               <div className="absolute flex flex-col items-start top-[20%] md:top-[40%] md:left-45 text-white">
+                  <p className="text-2xl font-semibold w-full md:w-auto">
+                     Para ocasioes especiais
+                  </p>
+                  <h1 className="text-6xl font-bold w-full">Viscose liso</h1>
+                  <h2 className="text-xl w-full md:w-auto md:mb-0 ">
                      Vestidos de viscoise falase opasdklaskdlas
                   </h2>
-                  <ButtonsCarrosel
-                     button1Text="Ver coleção"
-                     button2Text="Saiba mais sobre Viscose"
-                  />
+                  <div className="w-full items-center justify-center">
+                     <ButtonsCarrosel
+                        button1Text="Ver coleção"
+                        button2Text="Saiba mais sobre Viscose"
+                     />
+                  </div>
                </div>
                <img
                   src="https://png.pngtree.com/thumb_back/fw800/background/20240731/pngtree-beautiful-bride-in-long-white-dress-wet-hair-lying-on-a-image_15937018.jpg"
@@ -72,7 +78,7 @@ export function Carrosel() {
 export function ButtonsCarrosel({ button1Text, button2Text }) {
    return (
       <>
-         <div className="flex gap-4 mt-4 font-semibold">
+         <div className="flex gap-4 mt-45 md:mt-4 font-semibold text-sm md:text-[16px] md:justify-start justify-center">
             <button className="bg-yellow-600 cursor-pointer p-3 rounded-md">
                {button1Text}
             </button>
