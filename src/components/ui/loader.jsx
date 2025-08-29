3ecd94f89784feea1af4541473e8388b53c963cf
Kaua <kauacz04coc@gmail.com>
2025-08-29 01:03:35 -0300

    Adicionado novo loader

---
+++ src/components/ui/loader.jsx
@@ -0,0 +1,5 @@
+export default function ClassicLoader() {
+   return (
+      <div className=" border-amber-600 flex h-10 w-10 animate-spin items-center justify-center rounded-full border-5 border-t-transparent"></div>
+   );
+}
